72c80d429b2e2886579b87c3e1d0353e420619da
Daniil Tsivinsky <daniil@tsivinsky.com>
2025-05-20 22:50:29 +0300

    feat: delete all and move to next.js

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+export default function Home() {
+  return (
+    <div className="m-2">
+      <h1 className="text-4xl font-semibold">Dan Tsivinsky</h1>
+      <p className="text-xl mt-2">
+        This website is under construction, maybe I will finish it. For now you
+        can go to{" "}
+        <a
+          href="https://github.com/tsivinsky"
+          rel="me"
+          className="text-blue-500 underline text-lg"
+        >
+          my github
+        </a>
+        . It has some stuff.
+      </p>
+    </div>
+  );
+}
