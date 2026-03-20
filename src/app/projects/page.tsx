6dc9614d9f3c4d519475a7074e307c1fd846e1de
Daniil Tsivinsky <daniil@tsivinsky.com>
2026-03-20 15:55:03 +0300

    add 2 new projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -8,6 +8,23 @@ export default function Projects() {
       <h2 className="mt-5">Projects</h2>
       <div className="mt-3 flex flex-col gap-2 *:border-t *:pt-2">
         <Project
+          name="oauth2-proxy"
+          repo="https://git.tsivinsky.com/tsivinsky/oauth2-proxy"
+        >
+          Oauth2 proxy that allows user to make requests to apis behind oauth2.
+          It saves auth tokens and refreshes them when needed.
+        </Project>
+
+        <Project
+          name="tvqueue"
+          repo="https://git.tsivinsky.com/tsivinsky/tvqueue"
+        >
+          Project to watch for new torrents based on search query and show them
+          in a list. uses jackett to search for torrents and qbittorrent to
+          download them.
+        </Project>
+
+        <Project
           name="use-confirm"
           repo="https://github.com/tsivinsky/use-confirm"
         >
